f8aaf1b7de4a1697586da8a2f62ba80618afeab2
Josh Beard <josh@jlbeard.com>
2019-02-01 15:55:53 -0500

    More test client updates to parse additional server messaging

--- main.lua
+++ main.lua
@@ -17,41 +17,36 @@ host = enet.host_create();
 server = {};
 
 connected_time = 0;
-time_since_last_message = 0;
+time_since_last_ping = 0;
 messages_sent = 0;
 last_message_received = "";
-game_object = {};
-rnd_str = randomString(32);
+last_message_type_sent = "";
+client_id = "";
+peers = {};
 
 function love.load()
     server = host:connect("localhost:9521");
-    host:service(100);
-    game_object.client_id = rnd_str;
+    event = host:service(100);
 end
 
 function love.update(dt)
     if server:state() == "connected" then
         connected_time = connected_time + dt;
-        time_since_last_message = time_since_last_message + dt;
+        time_since_last_ping = time_since_last_ping + dt;
 
         local event = host:service();
 
-        if event then
-            if event.type == "receive" then
-                response_object = json.decode(event.data);
-                last_message_received = response_object.response_text or "";
-            end
-        end
+        process_event(event);
 
-        if time_since_last_message > 2 then
+        if time_since_last_ping > 5 then
+            local request_message = {};
 
-            game_object.message_type = 12;
+            request_message.client_id = client_id;
+            request_message.message_type = "ping";
 
-            local go = json.encode(game_object);
-            server:send(go);
+            send_to_server(request_message);
 
-            time_since_last_message = 0;
-            messages_sent = messages_sent + 1;
+            time_since_last_ping = 0;
         end
     
         if connected_time > 100 then
@@ -64,7 +59,51 @@ function love.draw()
     love.graphics.print("Status: " .. server:state(), 50, 20);
     love.graphics.print("Connected Time: " .. connected_time, 50, 40);
     love.graphics.print("Messages Sent: " .. messages_sent, 50, 60);
-    love.graphics.print("Time Since Last Message: " .. time_since_last_message, 50, 80);
-    love.graphics.print("Last Message Received: " .. last_message_received, 50, 100);
-    love.graphics.print("Client ID: " .. rnd_str, 50, 120);
+    love.graphics.print("Time Since Last Ping: " .. time_since_last_ping, 50, 80);
+    love.graphics.print("Last Message Type Sent: " .. last_message_type_sent, 50, 100);
+    love.graphics.print("Last Message Type Received: " .. last_message_received, 50, 120);
+    love.graphics.print("Client ID: " .. client_id, 50, 140);
+end
+
+function process_event(event) 
+    if event then
+        if event.type == "receive" then
+            response_object = json.decode(event.data);
+
+            if response_object.type then
+                if response_object.type == "addpeer" then
+                    last_message_received = "addpeer";
+                elseif response_object.type == "peermove" then
+                    last_message_received = "peermove";
+                elseif response_object.type == "pong" then
+                    last_message_received = "pong";
+                elseif response_object.type == "connect" then
+                    last_message_received = "connect";
+                    client_id = response_object.clientId;
+                    peers = response_object.peers;
+
+                    -- for key, value in pairs(peers) do
+                    --     for subkey, subvalue in pairs(value) do
+                    --         print(subkey);
+                    --     end
+                    -- end
+                end
+            else
+                --last_message_received = response_object.response_text or "";
+            end
+        elseif event.data then 
+            last_message_received = event.data;
+        end
+    end
+end
+
+function send_to_server(game_object) 
+
+    if game_object and game_object.message_type then
+        last_message_type_sent = game_object.message_type;
+    end
+
+    local go = json.encode(game_object);
+    server:send(go);
+    messages_sent = messages_sent + 1;
 end
